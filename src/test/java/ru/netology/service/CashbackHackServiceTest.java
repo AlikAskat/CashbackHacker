f16598a630361ce992305715194810da993d5d01
Alik Rakhmatullin <a-r-b@bk.ru>
2020-05-15 12:35:46 +0500

    Change file CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -8,18 +8,18 @@ class CashbackHackServiceTest {
     @Test
     void remain() {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 1000;
+        int amount = 900;
         int actual = cashbackHackService.remain(amount);
-        int expected = 10;
+        int expected = 100;
         assertEquals(expected, actual);
     }
 
     @Test
     void calculateCashbackHack() {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 2000;
+        int amount = 1000;
         int actual = cashbackHackService.remain(amount);
-        int expected = 1000;
+        int expected = 0;
         assertEquals(expected, actual);
     }
 }
