e2b95befb8b34184dacffe5f19149334f5660f40
Alik Rakhmatullin <a-r-b@bk.ru>
2020-05-22 15:50:10 +0500

    Update file CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
     @Test
-    void remain() {
+    void calculateCashbackHack900() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 900;
         int actual = cashbackHackService.remain(amount);
@@ -15,9 +15,18 @@ class CashbackHackServiceTest {
     }
 
     @Test
-    void calculateCashbackHack() {
+    void calculateCashbackHack1000() {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 1000;
+        int amount = 1001;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void calculateCashbackHack0() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 0;
         int actual = cashbackHackService.remain(amount);
         int expected = 0;
         assertEquals(expected, actual);
